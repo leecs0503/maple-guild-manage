939a3335647900697495ce79231439d229a2837d
lee changsu <leecs0503@naver.com>
2023-08-27 10:40:51 +0900

    feat: 웹서버, dataplane 기본 구현

--- maple_guild_manage_api_server/server/server.py
+++ maple_guild_manage_api_server/server/server.py
@@ -1,3 +1,28 @@
-class Server:
+import fastapi
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from ..errors import InvalidInput, invalid_input_handler
+
+class DataPlane:
     def __init__(self):
         pass
+
+class WebServer:
+    def __init__(
+        self,
+        dataplane: DataPlane,
+    ):
+        self.dataplane = dataplane
+
+    def create_application(self) -> FastAPI:
+        return FastAPI(
+            title="MapleGuildManageWebServer",
+              routes=[
+                # Server Liveness API returns 200 if server is alive.
+                APIRoute(r"/", self.dataplane.live),
+                # Metrics
+
+            ], exception_handlers={
+                InvalidInput: invalid_input_handler,
+            }
+        )
